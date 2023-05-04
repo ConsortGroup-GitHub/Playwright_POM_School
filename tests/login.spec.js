2bb68f400eabb3130a53fcf77c856ae71a58126b
ConsortGroup-GitHub <GSD.DLG.DOSol.GitHub@consort-group.com>
2023-05-04 18:05:11 +0200

    First Commit

---
+++ tests/login.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login'
+
+test('Login test', async ({ page }) => {
+
+  const Login = new LoginPage(page)
+
+  await page.goto('https://www.saucedemo.com/');
+  await Login.login('standard_user', 'secret_sauce')
+
+});
+
